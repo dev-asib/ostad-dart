6a558eb5828870f5f901f147704497440c4fc4be
dev_asib <mail.asib.com@gmail.com>
2024-02-16 00:13:26 +0600

    Ostad Dart Programming Langugage

---
+++ bin/firstProgram.dart
@@ -0,0 +1,8 @@
+void main(){
+  print('Hello Dart');
+  print(123);
+  print(true);
+  print("50");
+  print("""Dart Programming Language""");
+  print('''Bangladesh''');
+}
--- bin/ostad_dart_programming.dart
+++ bin/ostad_dart_programming.dart
@@ -1,6 +1,3 @@
-import 'package:ostad_dart_programming/ostad_dart_programming.dart' as ostad_dart_programming;
-
-void main(List<String> arguments) {
-  print('Hello world: ${ostad_dart_programming.calculate()}!');
-  print("Hello Asib");
+void main(){
+  print("Hello World!");
 }
