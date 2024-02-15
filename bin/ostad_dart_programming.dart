741f08170b7d450be3d41aa854cff2f3e482bb63
dev_asib <mail.asib.com@gmail.com>
2024-02-15 23:48:30 +0600

    Ostad Dart Programming Langugage

---
+++ bin/ostad_dart_programming.dart
@@ -0,0 +1,5 @@
+import 'package:ostad_dart_programming/ostad_dart_programming.dart' as ostad_dart_programming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ostad_dart_programming.calculate()}!');
+}
---
+++ test/ostad_dart_programming_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_dart_programming/ostad_dart_programming.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
